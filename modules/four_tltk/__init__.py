6f1e920b1cee07a029988aa6f6b5bc613fa9b88a
Matthew Dupree <mattjdupree@gmail.com>
2022-03-24 11:27:44 -0700

    Add untranslated line to untranslated.txt output.

--- modules/four_tltk/__init__.py
+++ modules/four_tltk/__init__.py
@@ -1,45 +1,28 @@
 import renpy
+from renpy import ast
 import io
 
-def make_untranslated_txt(language):#, min_priority, max_priority, common_only):
+def get_untranslated_info_line(tobj):
+    contents = ""
+    if tobj.block and (tobj.block[0].translatable or isinstance(tobj.block[0], ast.Say)):
+        contents = tobj.block[0].get_code()
+
+    return u'{}:{}:\t\t{}\t:\t{}\n'.format(tobj.filename, tobj.linenumber, tobj.identifier, contents)
+
+def make_untranslated_txt(language):
     """
-    Prints a count of missing translations for `language`.
+    Prints a list of missing translations for `language`.
     """
 
     translator = renpy.game.script.translator
 
     missing_translates = set()
 
-    # for filename in renpy.translation.generation.translate_list_files():
-    #     print("Checking translations in {}".format(filename))
-    #     for _, t in translator.file_translates[filename]:
-    #         if (t.identifier, language) not in translator.language_translates:
-    #             missing_translates.add(t.identifier)
-    #         else:
-    #             print(t.identifier,'is translated',t.identifier == translator.language_translates[(t.identifier, language)].identifier)
     for tlblockid, translate in translator.default_translates.items():
         if (tlblockid, language) not in translator.language_translates:
-            missing_translates.add((translate.filename,translate.linenumber,tlblockid))
-
-    # missing_strings = set()
-
-    # stl = renpy.game.script.translator.strings[language]  # @UndefinedVariable
-
-    # strings = renpy.translation.scanstrings.scan(common_only=False)
-
-    # for s in strings:
-
-    #     tlfn = renpy.translation.generation.translation_filename(s)
-
-    #     if tlfn is None:
-    #         continue
-
-    #     if s.text in stl.translations:
-    #         continue
-
-    #     missing_strings.add(s.text)
-
+            missing_translates.add(translate)
 
     with io.open('untranslated.txt','w',encoding='utf-8') as f:
-        f.write(u'\n'.join(sorted(map(lambda t: u':'.join(map(unicode,t)),missing_translates))))
+        for line in sorted(map(get_untranslated_info_line, missing_translates)):
+            f.write(line)
 
